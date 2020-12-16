5af885fcf57d99b01dfcff03b17814bd89273cde
tockacrta <boris@tockacrta.com>
2020-12-16 20:36:36 +0100

    Log install fix

--- installWindowsServiceLogListener.js
+++ installWindowsServiceLogListener.js
@@ -2,9 +2,9 @@ var Service = require('node-windows').Service;
  
 // Create a new service object
 var svc = new Service({
-  name:'isidor2',
-  description: 'Arhiva isidor2',
-  script: __dirname + '\\index.js',
+  name:'isidor2LogListener',
+  description: 'Log listener arhiva isidor2',
+  script: __dirname + '\\logListener.js',
   nodeOptions: [
     '--harmony',
     '--max_old_space_size=4096'
